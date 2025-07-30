64107c95fa9fb0861256a14f9e2785f5850c9a03
Zein Kassem <zein.s.kassem@gmail.com>
2025-07-31 00:04:02 +0300

    fix path for auth routes

--- frontend/src/routes/authRoutes.jsx
+++ frontend/src/routes/authRoutes.jsx
@@ -1,9 +1,7 @@
 import { LoginPage } from "../features/Auth/pages/LoginPage";
 import { RegisterPage } from "../features/Auth/pages/RegisterPage";
 
-const authRoutes = [
+export const authRoutes = [
   { path: "/login", element: <LoginPage /> },
   { path: "/register", element: <RegisterPage /> },
 ];
-
-export default authRoutes;
