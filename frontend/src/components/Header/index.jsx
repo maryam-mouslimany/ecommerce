8345834d69bf4bd9ad5674d07c8e70c3ce9dac31
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-07-31 14:09:19 +0300

    creating the header css module

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import styles from "./index.module.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className="header">
+        <div className={styles.heading}>
+          <span className={styles.his}>His</span>
+          <span className={styles.ampersand}>&amp;</span>
+          <span className={styles.hers}>Hers</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
