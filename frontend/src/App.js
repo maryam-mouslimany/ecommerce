29bd03811da8cb2111a360439107e49e94a0062d
Zein Kassem <zein.s.kassem@gmail.com>
2025-07-31 15:08:07 +0300

    product card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { Card } from "./components/Card";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
+      <Card />
     </div>
   );
 }
--- frontend/src/components/Caption/index.jsx
+++ frontend/src/components/Caption/index.jsx
@@ -1,10 +1,10 @@
 import styles from "./index.module.css";
 import clsx from "clsx";
 
-export const Caption = (variant) => {
+export const Caption = ({ variant, caption }) => {
   const className = clsx(
     styles.caption,
     variant && styles[`caption--${variant}`]
   );
-  return <div className={className}>index</div>;
+  return <div className={className}>{caption}</div>;
 };
