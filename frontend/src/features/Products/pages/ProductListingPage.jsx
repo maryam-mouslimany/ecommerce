8e83ef87db4056762779468284fb1e564ca1deb2
Zein Kassem <zein.s.kassem@gmail.com>
2025-08-01 00:06:24 +0300

    page product listing

---
+++ frontend/src/features/Products/pages/ProductListingPage.jsx
@@ -0,0 +1,3 @@
+export const ProductListingPage = () => {
+  return <div>ProductListingPage</div>;
+};
--- frontend/src/routes/authRoutes.js
+++ frontend/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import { LoginPage } from "../features/Auth/pages/LoginPage";
 import { RegisterPage } from "../features/Auth/pages/RegisterPage";
 import HomePage from "../features/Home/pages/HomePage";
+import { ProductListingPage } from "../features/Products/pages/ProductListingPage";
 
 export const authRoutes = [
   { path: "/login", element: <LoginPage /> },
   { path: "/register", element: <RegisterPage /> },
   { path: "/Home", element: <HomePage /> },
+  { path: "/product-list", element: <ProductListingPage /> },
 ];
