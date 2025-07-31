d8df35fb9efb3cbd169170bd2f7340cdf6941838
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-07-31 18:44:44 +0300

    Creating and styling the header component

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,17 +1,55 @@
 import React from "react";
 import styles from "./index.module.css";
+import { Link } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+import { FaShoppingCart } from "react-icons/fa";
+import { LuBellRing } from "react-icons/lu";
 
 const Header = () => {
   return (
-    <>
-      <div className={styles.header}>
-        <div className={styles.heading}>
-          <span className={styles.his}>His</span>
-          <span className={styles.ampersand}>&amp;</span>
-          <span className={styles.hers}>Hers</span>
+    <div className={styles.header}>
+      <div className={styles.heading}>
+        <span className={styles.his}>His</span>
+        <span className={styles.ampersand}>&amp;</span>
+        <span className={styles.hers}>Hers</span>
+      </div>
+
+      <div className={styles.menu}>
+        <div className={styles.main_menu}>
+          <ul>
+            <li>
+              <Link to="#">Home</Link>
+            </li>
+            <li>
+              <Link to="#">Products</Link>
+            </li>
+            <li>
+              <Link to="#">Menu</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.second_menu}>
+          <ul>
+            <li>
+              <Link to="#">
+                <CgProfile />
+              </Link>
+            </li>
+            <li>
+              <Link to="#">
+                <FaShoppingCart />
+              </Link>
+            </li>
+            <li>
+              <Link to="#">
+                <LuBellRing />
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- frontend/src/features/Auth/pages/LoginPage.jsx
+++ frontend/src/features/Auth/pages/LoginPage.jsx
@@ -1,9 +1,7 @@
 import { LoginForm } from "../../Auth/components/LoginForm";
-import Header from "../../../components/Header";
 export const LoginPage = () => {
   return (
     <>
-      <Header />
       <LoginForm />
     </>
   );
