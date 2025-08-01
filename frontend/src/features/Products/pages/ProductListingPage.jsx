63ce1e06ee6665c7591e4c29a4b42b79f51e64cc
Zein Kassem <zein.s.kassem@gmail.com>
2025-08-01 21:52:21 +0300

    filter product list

--- frontend/src/features/Products/pages/ProductListingPage.jsx
+++ frontend/src/features/Products/pages/ProductListingPage.jsx
@@ -4,45 +4,30 @@ import { useState } from "react";
 import { Card } from "../../../components/Card";
 import { Modal } from "../../../components/Modal";
 import { CardDetails } from "../components/CardDetails";
-
+import { FilterComponent } from "../components/ProductFilter";
 export const ProductListingPage = ({ title, price }) => {
   const [selectedProduct, setSelectedProduct] = useState(null);
-  return (
-    <div className={styles.container}>
-      {products.map((product) => (
-        // <div key={product.id} style={{ marginBottom: "1.5rem" }}>
-        //   <h3>{product.name}</h3>
-        //   <p>Brand: {product.brand.name}</p>
-        //   <p>Category: {product.category.name}</p>
-        //   <p>Gender: {product.gender}</p>
-        //   <img
-        //     src={product.images[0]?.url}
-        //     alt={product.name}
-        //     style={{ width: "150px", height: "auto" }}
-        //   />
-        //   <ul>
-        //     {product.variants.map((variant) => (
-        //       <li key={variant.id}>
-        //         {variant.size_ml}ml — ${variant.price} ({variant.stock} in
-        //         stock)
-        //       </li>
-        //     ))}
-        //   </ul>
-        //   <p>Accords: {product.accords.map((a) => a.name).join(", ")}</p>
-        // </div>
 
-        <Card
-          title={product.name}
-          price={`$${product.variants[0]?.price}`}
-          onViewMore={() => setSelectedProduct(product)}
-        />
-      ))}
-      <Modal
-        isOpen={!!selectedProduct}
-        onClose={() => setSelectedProduct(null)}
-      >
-        {selectedProduct && <CardDetails product={selectedProduct} />}
-      </Modal>
+  return (
+    <div>
+      <div className={styles.parentContainer}>
+        <FilterComponent />
+      </div>
+      <div className={styles.container}>
+        {products.map((product) => (
+          <Card
+            title={product.name}
+            price={`$${product.variants[0]?.price}`}
+            onViewMore={() => setSelectedProduct(product)}
+          />
+        ))}
+        <Modal
+          isOpen={!!selectedProduct}
+          onClose={() => setSelectedProduct(null)}
+        >
+          {selectedProduct && <CardDetails product={selectedProduct} />}
+        </Modal>
+      </div>
     </div>
   );
 };
