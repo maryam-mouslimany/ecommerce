f6e509af035b546875df34452e42e7c80d560987
Zein Kassem <zein.s.kassem@gmail.com>
2025-08-02 18:49:59 +0300

    fix merge in authRoute

--- frontend/src/routes/authRoutes.js
+++ frontend/src/routes/authRoutes.js
@@ -3,12 +3,9 @@ import { RegisterPage } from "../features/Auth/pages/RegisterPage";
 import HomePage from "../features/Home/pages/HomePage";
 import { ProductListingPage } from "../features/Products/pages/ProductListingPage";
 import { OrderConfirmation } from "../features/OrderConfirmation/pages";
-<<<<<<< HEAD
 import CreateProduct from "../features/AdminProductManagement/pages/CreateProduct";
 import ViewProducts from "../features/AdminProductManagement/pages/ViewProducts";
-=======
 import { OrderHistoryPage } from "../features/OrderHistory/pages/OrderHistory";
->>>>>>> feature/order-history
 
 import TheyPage from "../features/Home/pages/TheyPage";
 
@@ -18,12 +15,9 @@ export const authRoutes = [
   { path: "/Home", element: <HomePage /> },
   { path: "/product-list", element: <ProductListingPage /> },
   { path: "/order-confrimation", element: <OrderConfirmation /> },
-<<<<<<< HEAD
   { path: "/admin-view-products", element: <ViewProducts /> },
   { path: "/admin-create-products", element: <CreateProduct /> },
-=======
   { path: "/order-history", element: <OrderHistoryPage /> },
->>>>>>> feature/order-history
 
   { path: "/They", element: <TheyPage /> },
 ];
