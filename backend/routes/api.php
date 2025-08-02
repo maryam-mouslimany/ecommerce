520cb4990948f235ae6280c31550f8275cb94980
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-08-02 18:58:09 +0300

    creating api to get orders admin

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,23 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
 
 use App\Http\Controllers\AdminController;
 
-Route::group(["prefix" =>"v0.1"], function(){
-    Route::group(["middleware" => "auth:api"], function(){
-                Route::group(["prefix" => "user"], function(){        
-                    
-                        });
-                Route::group(["prefix" => "admin"], function(){
-            Route::get("/getOrders", [AdminController::class, "getOrders"]);
-         });
-    });
-   
+Route::group(["prefix" => "admin"], function(){
+Route::get("/getOrder", [AdminController::class, "getOrder"]);
+});
+                // Protected route to get authenticated user
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
-    Route::group(["prefix" => "guest"], function(){
-        Route::post("/login", [AuthController::class, "login"]);
-        Route::post("/register", [AuthController::class, "register"]);
-     });
+// Authentication Routes
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 });
